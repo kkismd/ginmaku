cbe26129ab18e37d5f7c198830f9b730590542dc
SHIMADA Keiki <shimada.cake@gmail.com>
2012-11-11 01:08:45 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ginmaku::Application.config.secret_token = '1d36ab0bf3b9c74d32785b8c58583839270c02fe7f421dafc34d2ed85f92f60e35f4f266af738b50f70e32acf904166d62914b323a5b38d41f86f4f886b0893f'
