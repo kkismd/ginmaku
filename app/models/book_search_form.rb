1c8a16dcc3f40e477b64508795771b5ecd4f6ed0
SHIMADA Keiki <shimada.cake@gmail.com>
2013-03-03 02:57:57 +0900

    change to search-form model

--- app/models/book_search_form.rb
+++ app/models/book_search_form.rb
@@ -4,12 +4,36 @@ class BookSearchForm
 
   attr_accessor :n, :c, :vf, :vt, :lang
 
+  LANG_BOTH = 1
+  LANG_JAPANESE = 2
+  LANG_ENGLISH = 3
+
   def initialize(params)
     self.n = params[:n] if params
     self.c = params[:c] if params
     self.vf = params[:vf] if params
     self.vt = params[:vt] if params
     self.lang = params[:lang] if params
+  end
+
+  def search
+    t = Book.arel_table
+    books =  Book.where(:book_name_id => n, :chapter => c)
+    case lang
+      when LANG_BOTH
+        books = books.where(t[:version].eq(1).or(t[:version]).eq(2))
+      when LANG_JAPANESE
+        books = books.where(:version => 1)
+      when LANG_ENGLISH
+        books = books.where(:version => 2)
+    end
+    if vf.present?
+      books = books.where(t[:verse].gteq(vf))
+    end
+    if vt.present?
+      books = books.where(t[:verse].lteq(vt))
+    end
+    books.order(t[:chapter].asc, t[:verse].asc)
   end
 
   def persisted?
