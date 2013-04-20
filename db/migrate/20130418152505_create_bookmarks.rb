3f8eb3ad7ab5bbf8d9785f22c12f3adc4cc064d0
SHIMADA Keiki <shimada.cake@gmail.com>
2013-04-20 21:08:12 +0900

    refs #7 bookmarkの基本となるテーブルと動作

--- db/migrate/20130418152505_create_bookmarks.rb
+++ db/migrate/20130418152505_create_bookmarks.rb
@@ -1,10 +1,10 @@
 class CreateBookmarks < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
-      t.string :name
-      t.string :controller
-      t.string :action
-      t.text :params
+      t.string :title
+      t.string :action_name
+      t.string :controller_name
+      t.text :params_value
 
       t.timestamps
     end
