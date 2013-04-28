5bb598af766cd1a4f5f6284b6609e3ae90442e6b
SHIMADA Keiki <shimada.cake@gmail.com>
2013-04-28 16:30:18 +0900

    ローマ字ルールを追加

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -6,7 +6,7 @@ module SongsHelper
         gsub(Song::KANJI_KANA){"<ruby>#{$1}<rt>#{$2}</rt></ruby>"}.
         gsub(Song::ENG_KANA){"<ruby>#{$1}<rt>#{$2}</rt></ruby>"}.
         gsub(Song::KANA_HIRA){"<ruby>#{$1}<rt>#{$2}</rt></ruby>"}.
-        gsub(/^([-A-Z 　'"!]+)$/){ "<span class='roman'>#{$1}</span>" }.
+        gsub(/^([-A-Z 　'"!,:]+)$/){ "<span class='roman'>#{$1}</span>" }.
         html_safe
   end
 
