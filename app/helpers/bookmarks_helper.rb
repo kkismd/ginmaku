454248e43a3f76dc5ab81578ee26db851708fbad
SHIMADA Keiki <shimada.cake@gmail.com>
2013-06-02 20:16:36 +0900

    日本語を幅2としたtruncate

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,5 +1,30 @@
+# coding: utf-8
 module BookmarksHelper
   def link_to_bookmark(bookmark)
-    link_to truncate(bookmark.title, length: 17), url_for(bookmark.raw_params)
+    link_to truncate_ja(bookmark.title, 29), url_for(bookmark.raw_params)
+  end
+
+  # 非ASCII文字を文字幅2として計算する truncate
+  def truncate_ja(str, limit)
+    omission = '…'
+    chars = str.split //
+
+    result = ''
+    result_len = 0
+    chars.each do |char|
+      if char.match /\p{ASCII}/
+        result_len += 1
+      else
+        result_len += 2
+      end
+
+      if result_len <= limit
+        result << char
+      else
+        result << omission
+        break
+      end
+    end
+    result
   end
 end
